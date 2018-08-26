7687cd37c55ffd125d70eff75f746d28443b02cf
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-08-26 15:34:47 -0500

    working

--- object.js
+++ object.js
@@ -1,1 +1,15 @@
 var ourDog = {
+   "name": "Camper",
+ //   "legs": 4,
+  //  "tails": 1,
+  //  "friends": ["everything!"]
+ // };
+  
+  // Only change code below this line.
+  
+  //var myDog = {
+  //  "name": "Treek",
+  //  "legs": 4,
+    //"tails": 1,
+    //"friends": ["Treek", "Kitty"]
+ //g };
