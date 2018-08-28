865d24cfd7cadba96752a0b749a01ecfde4f9011
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-08-27 21:12:56 -0500

    still understanding the theory of javascript

---
+++ objectsCombinations.js
@@ -0,0 +1,26 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+   { 'artist':'treek',
+     'title': 'myYear',
+      'release_year': 2018,
+      'formats': [
+        'cd',
+        'mp4',
+        'lp'
+      ],
+      'platium': true
+    }
+     
+
+     
+    ];
