dafc3ed26a1b71d88c5bb400f3f4cce09685779d
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-08-29 20:39:58 -0500

    this is an excerise

---
+++ arrayPushExcerise.js
@@ -0,0 +1,15 @@
+// Example
+var ourArray = [];
+// irrating and pushing out even numbers to the array/
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+// pushing out odd numbers
+for( var i = 1;i <10; i+= 2){
+  myArray.push(i);
+}
