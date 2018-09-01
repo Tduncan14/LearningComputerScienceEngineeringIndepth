c55a9469b77a3e67fdaa958be6d094abc742f437
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-01 16:43:18 -0500

    udemy courses

--- libraryFrame.js
+++ libraryFrame.js
@@ -5,7 +5,17 @@
     var greeter = function(firstName,lastName,language){
        return new greeter.init(firstName,lastName,language);    
     }
+
+    // creating a protoType
+    greeter.prototype ={};
     greeter.init = function(firstName,lastName,language){
         // setting up some default properties.
-        
+        // it is either language that is true / ir
+
+       var self = this;
+       self. firstName = firstName || '';
+       self.lastName = lastName|| '';
+       self.language = language || 'en';
+
+    }
 })(window,jQuery);
