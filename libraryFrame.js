15ef90582efd0ebc5354cc975d594b0eef273f02
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-01 16:38:26 -0500

    udemy courses

---
+++ libraryFrame.js
@@ -0,0 +1,11 @@
+// considered as safeCode/
+
+(function(global,$){
+
+    var greeter = function(firstName,lastName,language){
+       return new greeter.init(firstName,lastName,language);    
+    }
+    greeter.init = function(firstName,lastName,language){
+        // setting up some default properties.
+        
+})(window,jQuery);
