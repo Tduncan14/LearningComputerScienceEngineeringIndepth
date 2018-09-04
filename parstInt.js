bfb0cf547a9987cbd4a91d189f91e3accc7e5b9c
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-04 17:43:06 -0500

    more learning

---
+++ parstInt.js
@@ -0,0 +1,13 @@
+
+//a built in javascript function called parseInt();
+// turns a string into a number.
+// but if the number cannot be converted into a string. The function will return nan(not a number);
+
+function convertToInteger(str) {
+  
+    return parseInt(str);
+  }
+  
+  convertToInteger("56");
+
+  // will return the number 56
