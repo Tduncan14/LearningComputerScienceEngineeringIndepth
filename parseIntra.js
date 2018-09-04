a13a802758d2149ea2f6348c2d777cbab2c88427
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-04 17:57:29 -0500

    more learning

---
+++ parseIntra.js
@@ -0,0 +1,10 @@
+// radix is the second argrument to the parseInt function. That works on the binary numbers from 0 to 10 as our base
+
+function convertToInteger(str) {
+
+return parseInt(str,2)
+}
+
+convertToInteger("10011");
+
+console.log(convertToInteger("10011"));
